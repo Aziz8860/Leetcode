671b91b2cf287a1e369c6863b794985180f7091b
Aziz <muhaariq@gmail.com>
2024-02-09 21:18:23 +0700

    update contains duplicate

--- 217-contains-duplicate.py
+++ 217-contains-duplicate.py
@@ -13,5 +13,17 @@ def containsDuplicate(nums: list[int]) -> bool:
     else:
         print('false')
         return False
+    
+
+    '''
+    # neetcode version
+    hashset = set()
+        
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
+    '''
         
 containsDuplicate([2,14,18,22, 22])

75ede83e2791167628f57bc5797f981c4b31cf1f
Aziz <muhaariq@gmail.com>
2024-02-25 13:39:32 +0700

    125-valid-palindrome update

--- 125-valid-palindrome.py
+++ 125-valid-palindrome.py
@@ -13,9 +13,15 @@ class Solution:
         else:
             return False
         
-    # cara bang fikri
+    # neetcode's solution 1
+    # newStr = ""
+    # for c in s:
+    #     if c.isalnum():
+    #         newStr += c.lower()
+    # return newStr == newStr[::-1]
+        
+    # cara bang fikri (paling oke)
     # left, right = 0, len(s) - 1
-
     # while left < right:
     #     if not s[left].isalnum():
     #         left += 1
@@ -28,3 +34,22 @@ class Solution:
     #         right -= 1
     # return True
         
+    # neetcode's solution 2 (kalo gamau pake isalnum() jadi pake ascii)
+    # l, r = 0, len(s) - 1
+
+    # if you want to call another function inside of an object you have to use self keyword
+    # while l < r:
+    #     while l < r and not self.alphaNum(s[l]):
+    #         l += 1
+    #     while r < l and not self.alphaNum(s[r]):
+    #         r -= 1
+    #     if s[l].lower() != s[r].lower():
+    #         return False
+    #     l, r = l + 1, r - 1
+    # return True
+        
+    # outside function
+    # def alphaNum(self, c):
+    #   return (ord('A') <= ord(c) <= ord('Z') or
+    #           ord('a') <= ord(c) <= ord('z') or
+    #           ord('0') <= ord(c) <= ord('9'))

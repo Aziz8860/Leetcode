0e5f762224525009924a8bd5fda3b076ac1482f5
Aziz <muhaariq@gmail.com>
2024-03-16 22:49:29 +0700

    206-reverse-linked-list (update)

--- 206-reverse-linked-list.py
+++ 206-reverse-linked-list.py
@@ -1,10 +1,33 @@
 def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
     # iterative approach
-    prev, curr = None, head
+    # prev, curr = None, head
 
-    while curr:
-        nxt = curr.next
-        curr.next = prev
-        prev = curr
-        curr = nxt
-    return prev
+    # while curr:
+    #     nxt = curr.next
+    #     curr.next = prev
+    #     prev = curr
+    #     curr = nxt
+    # return prev
+
+    # recursive approach
+    # if not head: # maskudnya if head is null, return null
+    #     return None
+
+    # newHead = head
+    # if head.next:
+    #     newHead = self.reverseList(head.next)
+    #     head.next.next = head # reversing the link between the next node and head
+    # head.next = None
+
+    # return newHead
+
+    # better recursive approach by Extremesarova
+    def reverse(cur, prev):
+        if cur is None:
+            return prev
+        else:
+            next = cur.next
+            cur.next = prev
+            return reverse(next, cur)
+
+    return reverse(head, None)

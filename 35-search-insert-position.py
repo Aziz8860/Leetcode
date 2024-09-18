9f97cc5b60da7681337f000944ea1f3c8490dcbd
Aziz <muhaariq@gmail.com>
2024-09-18 11:31:32 +0700

    leetcode 27 and 35

---
+++ 35-search-insert-position.py
@@ -0,0 +1,20 @@
+def searchInsert(nums: list[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        # If target is greater, ignore left half
+        if nums[mid] < target:
+            low = mid + 1
+
+        # If target is smaller, ignore right half
+        elif nums[mid] > target:
+            high = mid - 1
+
+        # means target is present at mid
+        else:
+            return mid
+    return low

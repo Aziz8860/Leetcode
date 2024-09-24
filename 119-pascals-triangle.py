a7fc95f7161b0f5632b85c3e3711ea988538b0e6
Aziz <muhaariq@gmail.com>
2024-09-24 23:48:31 +0700

    leetcode update

---
+++ 119-pascals-triangle.py
@@ -0,0 +1,11 @@
+def getRow(rowIndex: int) -> list[int]:
+    res = [[1]]
+
+    for i in range(1, rowIndex + 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1): # bisa juga range(len(temp) - 1)
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res[rowIndex]
+        
